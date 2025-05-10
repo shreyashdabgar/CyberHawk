59cecaff58a15f5b290f97bb4d339b07e8a29bbf
shreyashdabgar <dabgarshreyash199@gmail.com>
2025-05-10 17:22:48 +0530

    testing model successfully

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from netwoksecurity.pipeline.training_pipeline import TrainingPipeline
 from netwoksecurity.utills.main_utils.utils import *
 from netwoksecurity.constents.traning_pipeline import * 
 from netwoksecurity.entities.config_entity import *
+from netwoksecurity.utills.ml_utills.model.estimetor import networkmodel
 
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, File, UploadFile, requests
@@ -19,7 +20,8 @@ from fastapi.responses import Response
 from uvicorn import run as app_run
 from starlette.responses import RedirectResponse
 import pandas as pd 
-import numpy as np 
+import numpy as np
+from fastapi.templating import Jinja2Templates 
 
 
 ca = certifi.where()
@@ -45,6 +47,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+''' accesing templetes '''
+templates = Jinja2Templates(directory="./templates")
+
 @app.get("/", tags=["authentication"])
 async def index():
     return RedirectResponse(url="/docs")
@@ -61,5 +66,26 @@ async def train():
     except Exception as e:
         raise CustomException(e)
     
+
+
+
+@app.post("/predict")
+async def predict(request: requests.Request, file : UploadFile= File(...)):
+    try:
+        logging.info("prediiction is strated")
+        df = pd.read_csv(file.file)
+        preprocesser= load_obj("final_model\preprocesser.pkl")
+        model= load_obj("final_model\model.pkl")
+        network_model = networkmodel(preprocesser, model)
+        prediction = network_model.predict(df)
+        print(prediction)
+        df['prediction'] = prediction
+        df.to_csv('output/prediction.csv')
+        table = df.to_html(classes="table table-striped")
+        return templates.TemplateResponse("table.html", {"request" : request, "table" : table})
+
+    except Exception as e :
+        raise CustomException(e)
+
 if __name__ == "__main__":
     app_run(app, host= "localhost", port = 8000)
